43812f8fce1acfa0c5b252c17439205c93db0d3a
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-13 19:27:42 +0530

    Add CSS to Home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { BsFillCartFill } from 'react-icons/bs'
-import '../styles/navbar.css'
+import '../styles/navbar.css';
 
 const number = 12;
 
