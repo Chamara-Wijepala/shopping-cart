05649f6ef4511f13d153722686451b5e0298f8ac
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-13 16:29:09 +0530

    Add Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <h1>PokéMart</h1>
+      </div>
+      <div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
