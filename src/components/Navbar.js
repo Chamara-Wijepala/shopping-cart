fb4160ac642966590d4132e32bf3eb04155dee74
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-13 18:36:13 +0530

    Add CSS to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { BsFillCartFill } from 'react-icons/bs'
+import '../styles/navbar.css'
+
+const number = 12;
 
 const Navbar = () => {
   return (
     <nav>
-      <div>
-        <h1>PokéMart</h1>
-      </div>
-      <div>
+      <h1>PokéMart</h1>
+      <div id="NavItems">
         <Link to="/">Home</Link>
         <Link to="/products">Products</Link>
+        <p>{number}<BsFillCartFill/></p>
       </div>
     </nav>
   );
