42e49caec358a00104b8e064ed935c16910096f3
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-13 21:22:47 +0530

    Add mock product card

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,20 @@
 import React from "react";
+import '../styles/products.css';
 
 const Products = () => {
   return (
     <div id="ProductList">
-      
+      <div className="Product">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png" alt=""></img>
+        <p className="Name">Product</p>
+        <p className="Price">$ 200</p>
+        <div className="AmountToAdd">
+          <button>-</button>
+          <input defaultValue={1}></input>
+          <button>+</button>
+        </div>
+        <button className="AddToCart">Add To Cart</button>
+      </div>
     </div>
   );
 };
