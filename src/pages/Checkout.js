77c189b8b810e7d642c0a21f8ecdd0d2d4c52a76
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-18 17:08:48 +0530

    Add CSS to Checkout

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,8 +1,12 @@
 import React from "react";
+import '../styles/checkout.css';
 
 const Checkout = () => {
   return (
     <div id="Checkout">
+      <div id="CheckoutList"></div>
+      <p id="TotalPrice">Total: $ 1200</p>
+      <button id="CheckoutButton">Checkout</button>
     </div>
   );
 };
