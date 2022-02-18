559bf2949851f2542883df7595ae58f723880322
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-18 18:35:56 +0530

    Add mockup of checkout item

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -4,7 +4,20 @@ import '../styles/checkout.css';
 const Checkout = () => {
   return (
     <div id="Checkout">
-      <div id="CheckoutList"></div>
+      <div id="CheckoutList">
+        <div className="CheckoutItem">
+          <div className="CheckoutItemQuantity">12</div>
+          <img
+            className="ItemImg"
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png"
+            alt=""
+          />
+          <div className="ItemInfo">
+            <p>Pokeball</p>
+            <p>$ 200</p>
+          </div>
+        </div>
+      </div>
       <p id="TotalPrice">Total: $ 1200</p>
       <button id="CheckoutButton">Checkout</button>
     </div>
