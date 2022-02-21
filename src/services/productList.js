457230cdea3c80b137e731d90f466777cbc70340
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-21 15:18:03 +0530

    Add id and quantity property to each object

--- src/services/productList.js
+++ src/services/productList.js
@@ -2,96 +2,134 @@ const productList = [
   {
     "name": "Poké Ball",
     "cost": 200,
+    "id": 1,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png"
   },
   {
     "name": "Great Ball",
     "cost": 600,
+    "id": 2,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/great-ball.png"
   },
   {
     "name": "Ultra Ball",
     "cost": 800,
+    "id": 3,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/ultra-ball.png"
   },
   {
     "name": "Potion",
     "cost": 200,
+    "id": 4,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/potion.png"
   },
   {
     "name": "Super Potion",
     "cost": 700,
+    "id": 5,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/super-potion.png"
   },
   {
     "name": "Hyper Potion",
     "cost": 1500,
+    "id": 6,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/hyper-potion.png"
   },
   {
     "name": "Max Potion",
     "cost": 2500,
+    "id": 7,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/max-potion.png"
   },
   {
     "name": "Full Restore",
     "cost": 3000,
+    "id": 8,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/full-restore.png"
   },
   {
     "name": "Revive",
     "cost": 2000,
+    "id": 9,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/revive.png"
   },
   {
     "name": "Antidote",
     "cost": 200,
+    "id": 10,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/antidote.png"
   },
   {
     "name": "Paralyze Heal",
     "cost": 200,
+    "id": 11,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/paralyze-heal.png"
   },
   {
     "name": "Awakening",
     "cost": 200,
+    "id": 12,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/awakening.png"
   },
   {
     "name": "Burn Heal",
     "cost": 200,
+    "id": 13,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/burn-heal.png"
   },
   {
     "name": "Ice Heal",
     "cost": 200,
+    "id": 14,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/ice-heal.png"
   },
   {
     "name": "Full Heal",
     "cost": 400,
+    "id": 15,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/full-heal.png"
   },
   {
     "name": "Escape Rope",
     "cost": 300,
+    "id": 16,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/escape-rope.png"
   },
   {
     "name": "Repel",
     "cost": 400,
+    "id": 17,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/repel.png"
   },
   {
     "name": "Super Repel",
     "cost": 700,
+    "id": 18,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/super-repel.png"
   },
   {
     "name": "Max Repel",
     "cost": 900,
+    "id": 19,
+    "quantity": 1,
     "sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/max-repel.png"
   }
 ];
