58d2c7bb1094ec11b705105130e2833466539b61
Chamara-Wijepala <mr.worf777@gmail.com>
2022-02-22 17:15:57 +0530

    Redeploy project

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Navbar {...{cart}} />
         <main>
           <Routes>
-            <Route path="/shopping-cart" element={<Home/>} />
+            <Route path="/" element={<Home/>} />
             <Route
               path="/products"
               element={<Products {...{cart, setCart}}/>}
